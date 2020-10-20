c67231c946c9f717bd11d7b91605aeb281116265
Tim Corley <tcorley215@gmail.com>
2020-10-19 20:39:13 -0400

    fetching all bugs

--- client/src/components/BugsList.js
+++ client/src/components/BugsList.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 const BugsList = () => {
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/bugs")
+      .then((res) => console.log(res.data));
+  });
   return (
     <div>
       <p>this is the bugs list component</p>
